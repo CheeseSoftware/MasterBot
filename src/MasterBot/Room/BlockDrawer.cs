d4c3beba462945295e74ce9e0ac696317a248904
ostkaka0 <john.ostkaka@gmail.com>
2014-05-13 12:22:39 +0200

    Blockdrawerpool bla

---
+++ src/MasterBot/Room/BlockDrawer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterBot.Room
+{
+    public class BlockDrawer
+    {
+        IBlockDrawerPool blockDrawerPool;
+        byte priority;
+        bool running = false;
+
+        public BlockDrawer(IBlockDrawerPool blockDrawerPool, byte priority = 0)
+        {
+            this.priority = priority;
+        }
+
+        public void Start()
+        {
+            blockDrawerPool.StartBlockDrawer(this);
+            running = true;
+        }
+
+        public void Stop()
+        {
+            blockDrawerPool.StopBlockDrawer(this);
+            running = false;
+        }
+
+        public void setPriority(byte priority)
+        {
+            this.priority = priority;
+        }
+
+        public byte Priority
+        {
+            get { return priority; }
+        }
+        
+    }
+}
---
+++ src/MasterBot/Room/BlockDrawerPool.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MasterBot.Room
+{
+    public class BlockDrawerPool : IBlockDrawerPool
+    {
+        SafeList<BlockDrawer> queuedBlockdrawers = new SafeList<BlockDrawer>();
+
+        public BlockDrawer CreateBlockDrawer(byte priority)
+        {
+            return new BlockDrawer(this, priority);
+        }
+
+        public void StartBlockDrawer(BlockDrawer blockDrawer)
+        {
+            for (int i = 0; i <= blockDrawer.
+        }
+
+        public void StopBlockDrawer(BlockDrawer blockDrawer)
+        {
+            for (int i = queuedBlockdrawers.Count - 1; i >= 0; i--)
+            {
+                if (queuedBlockdrawers[i] == blockDrawer)
+                    queuedBlockdrawers.RemoveAt(i);
+            }
+        }
+    }
+}
