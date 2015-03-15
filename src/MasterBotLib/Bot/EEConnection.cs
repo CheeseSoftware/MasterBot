3e0c872fc29a95a17f41eb65354e0a1a29abaec9
ostkaka <john.ostkaka@gmail.com>
2015-03-15 11:13:38 +0100

    .

---
+++ src/MasterBotLib/Bot/EEConnection.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PlayerIOClient;
+
+namespace MasterBot.Bot
+{
+    class EEConnection
+    {
+        Connection connection;
+    }
+}
